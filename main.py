e756a6aa07eea262caaab673b44274a888dbbdaa
TianbinLiu <89219591+TianbinLiu@users.noreply.github.com>
2022-04-05 15:21:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from flask import render_template
+
+# create a Flask instance
+app = Flask(__name__)
+
+
+@app.route("/")
+# map URL route for function below
+def index():
+    return render_template("index.html")
+  
 #Pranavi and Gigi
