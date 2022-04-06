eed909f5b313df28b6e4660351de5b5eaae810f2
gigi <gigikwan180@gmail.com>
2022-04-06 14:57:31 -0700

    pranavi&gigi

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 #Pranavi and Gigi
+
 from cruddy.app_crud import app_crud
+from cruddy.app_crud_api import app_crud_api
+
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    # runs the application on the repl development server
+    app.run(debug=True, port="5222")
