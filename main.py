ac1a7856fce226ec8ea260236655fbf39f028809
Pranavi <pranukurti@gmail.com>
2022-04-07 14:10:23 -0700

    first try of adding hack 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Pranavi and Gigi
+# Pranavi and Gigi
 from flask import render_template
 from __init__ import app
 
@@ -22,6 +22,7 @@ def authorize():
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     # runs the application on the repl development server
