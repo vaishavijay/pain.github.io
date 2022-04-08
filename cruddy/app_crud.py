487c6ca81849ca441cd3cdee476440e9357e03af
TianbinLiu <l1965921542@gmail.com>
2022-04-08 15:17:12 -0700

    fix phone number

--- cruddy/app_crud.py
+++ cruddy/app_crud.py
@@ -61,9 +61,10 @@ def crud_authorize():
         # validation should be in HTML
         user_name = request.form.get("user_name")
         email = request.form.get("email")
+        phone = request.form.get("phone")
         password1 = request.form.get("password1")
         password2 = request.form.get("password1")           # password should be verified
-        if authorize(user_name, email, password1):    # zero index [0] used as user_name and email are type tuple
+        if authorize(user_name, email, phone, password1):    # zero index [0] used as user_name and email are type tuple
             return redirect(url_for('crud.crud_login'))
     # show the auth user page if the above fails for some reason
     return render_template("authorize.html")
