82aeba6bc88aafd2e80034a5f46701247598e5a0
TianbinLiu <l1965921542@gmail.com>
2022-05-24 15:31:12 -0700

    add file page, add drawing box in note page

--- __init__.py
+++ __init__.py
@@ -12,7 +12,9 @@ These object will be used throughout project.
 # Setup of key Flask object (app)
 app = Flask(__name__)
 dbURI = 'sqlite:///myDB.db'
+UPLOAD_FOLDER = 'static/uploads/'
 # Setup SQLAlchemy object and properties for the database (db)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 app.config['SECRET_KEY'] = 'SECRET_KEY'
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from flask_login import login_required
 import json
 from __init__ import app, login_manager
 from cruddy.app_crud import app_crud
+from contenty.app_content import app_content
 from cruddy.app_crud_api import app_crud_api
 from notey.app_notes import app_notes
 from arty.app_art import app_art
 
 from cruddy.login import login, logout, signup
 
+app.register_blueprint(app_content)
 app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
 app.register_blueprint(app_notes)
